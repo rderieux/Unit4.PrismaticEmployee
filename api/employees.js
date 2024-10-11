7b99bd08c8358da322aaf32d1856e55d39894c11
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-11 12:41:26 -0600

    Add a route endpoint to GET all employees

--- api/employees.js
+++ api/employees.js
@@ -2,3 +2,12 @@ const express = require("express");
 const prisma = require("../prisma");
 const router = express.Router();
 module.exports = router;
+
+router.get("/", async (req, res, next) => {
+  try {
+    const employees = await prisma.employee.findMany();
+    res.json(employees);
+  } catch (error) {
+    next(error);
+  }
+});

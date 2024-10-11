e7653b4de272b123ffed2a29f2ef9a30ec3fa7db
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-11 11:17:01 -0600

    Import the employees api in the server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use("/employees", require("./api/employees"));
+
 // Logging middleware
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.originalUrl}`);

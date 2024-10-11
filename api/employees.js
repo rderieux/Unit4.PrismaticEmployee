4f9a9907e2177e848de7de5dc71dcbea443cb1dd
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-11 12:48:07 -0600

    Add a route endpoint to GET a single employee by id

--- api/employees.js
+++ api/employees.js
@@ -11,3 +11,22 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+router.get("/:id", async (req, res, next) => {
+  // Get id from request params
+  const { id } = req.params;
+
+  // Try catch get single employee by id
+  try {
+    const employee = await prisma.employee.findUnique({ where: { id: +id } });
+    // Guard case
+    if (employee) {
+      // Respond with json employee
+      res.json(employee);
+    } else {
+      next({ status: 404, message: `Employee with ${id} does not exist.` });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
